3393899cc81b08278d280f116d7218300b327bd8
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-08-02 11:55:51 -0700

    add HTML and css wireframes and JavaScript elements

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 // import functions and grab DOM elements
 
+// elements in the current poll div
+const questionDisplayEl = document.getElementsById('question-display');
+const answerOneDisplayEl = document.getElementById('current-answer-one');
+const answerTwoDisplayEl = document.getElementById('current-answer-two');
+const votesOneDisplayEl = document.getElementById('votes-one');
+const votesTwoDisplayEl = document.getElementById('votes-two');
+
+// elements in the poll input div
+const votesOneTallyUp = document.getElementById('answer-one-tally-up');
+const votesTwoTallyUp = document.getElementById('answer-two-tally-up');
+const votesOneTallyDown = document.getElementById('answer-one-tally-down');
+const votesTwoTallyDown = document.getElementById('answer-two-tally-down');
+
+// elements in the poll input div
+const questionInput = document.getElementById('enter-question');
+const answerOneInput = document.getElementById('answer-one-input');
+const answerTwoInput = document.getElementById('answer-two-input');
+  // submit button Element
+const = submitPollButtonEl = document.getElementById(''); 
+
 // let state
 
 // set event listeners 
